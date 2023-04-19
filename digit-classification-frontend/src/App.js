15c6088d1d39122d0f04fb84d5ad21b9a7c19821
Omkar Shirgaonkar <75793355+BulzEye@users.noreply.github.com>
2023-04-19 14:49:31 +0530

    leetle bit styling

--- digit-classification-frontend/src/App.js
+++ digit-classification-frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
 import Canvas from './components/Canvas';
+import styles from "./App.module.css";
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Container>
+                <h2 className={styles.intro_heading}>Draw the digit in the canvas below</h2>
                 <Canvas width={280} height={280}/>
             </Container>
         </div>
